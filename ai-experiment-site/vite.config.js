1e9a20ede3ecb45abadedab489b4a4b25b6d8d21
Michael Krog <mkrogdev@gmail.com>
2025-05-31 15:33:00 -0600

    add proper build and new main page pointer

--- ai-experiment-site/vite.config.js
+++ ai-experiment-site/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 // import process from 'node:process';
@@ -7,7 +7,7 @@ export default defineConfig(() => {
   // const env = loadEnv(mode, process.cwd(), '');
   
   return {
-    base: "/ai-experiment/",
+    base: '/ai-experiment',
     plugins: [tailwindcss(), react()],
     css: {
       devSourcemap: true
